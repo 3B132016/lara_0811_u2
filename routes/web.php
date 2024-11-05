f1115f9db4403d9dd5924373de8c08f0b2c45b65
TsengJay <tsengjay00@>
2024-11-05 23:16:58 +0800

    Name route as hello.index and add links in view

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,12 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('/', function() {
+    return view('welcome');
+});
 
 Route::get('hello/{name?}', function($name = 'Everybody') {
     return 'Hello, ' . $name;
-});
+})->name('hello.index');;
 
 
