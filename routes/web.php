ad9e0b8ad09d2e39d9889fd5d07a04273bd5428a
TsengJay <tsengjay00@>
2024-11-05 23:19:00 +0800

    Set up route with and without prefix for dashboard

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,13 @@ Route::get('hello/{name?}', function($name = 'Everybody') {
     return 'Hello, ' . $name;
 })->name('hello.index');;
 
+Route::get('dashboard', function() {
+    return 'dashboard';
+});
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('dashboard', function() {
+        return 'admin dashboard';
+    });
+});
 
