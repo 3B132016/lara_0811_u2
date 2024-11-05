dbdc22c7bcaae4e20593ecad0d39bd13a3fb3796
TsengJay <tsengjay00@>
2024-11-05 23:00:42 +0800

    Make name parameter optional with default greeting

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('hello/{name}', function($name) {
+Route::get('hello/{name?}', function($name = 'Everybody') {
     return 'Hello, ' . $name;
 });
+
 
